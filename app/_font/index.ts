aae419294ab0e30d0b06232144588b601f435bbd
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-08-15 23:50:22 +0330

    Install Font

---
+++ app/_font/index.ts
@@ -0,0 +1,71 @@
+import localFont from 'next/font/local'
+
+export const peydaFont = localFont({
+    src: [
+      {
+        path: './PeydaWeb-Thin.woff',
+        weight: '100',
+        style: 'normal',
+      },
+      {
+        path: './peydaWeb-extralight.woff',
+        weight: '200',
+        style: 'normal',
+      },
+      {
+        path: './PeydaWeb-light.woff',
+        weight: '300',
+        style: 'normal',
+      },
+
+      {
+        path: './PeydaWeb-Regular.woff',
+        weight: '400',
+        style: 'normal',
+      },
+
+
+      {
+        path: './PeydaWeb-Medium.woff',
+        weight: '500',
+        style: 'normal',
+      },
+
+
+
+      {
+        path: './PeydaWeb-Medium.woff',
+        weight: '500',
+        style: 'normal',
+      },
+
+
+      {
+        path: './PeydaWeb-SemiBold.woff',
+        weight: '600',
+        style: 'normal',
+      },
+
+
+      {
+        path: './PeydaWeb-Bold.woff',
+        weight: '700',
+        style: 'normal',
+      },
+
+
+      {
+        path: './PeydaWeb-ExtraBold.woff',
+        weight: '800',
+        style: 'normal',
+      },
+
+      {
+        path: './PeydaWeb-Black.woff',
+        weight: '900',
+        style: 'normal',
+      },
+
+
+    ],
+  })
