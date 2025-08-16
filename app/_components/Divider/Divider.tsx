3354241e532bbeff690c81dc4975ea476184e965
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-08-16 14:50:27 +0330

    Develope Divider

---
+++ app/_components/Divider/Divider.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Divider = () => {
+    return (
+        <div className='w-full h-[1px] bg-[#F4F4F4]'></div>
+    )
+}
--- app/_components/index.ts
+++ app/_components/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './SideBar/Sidebar'
 export { Header } from './Header/Header'
 export { Button } from './Button/Button'
+export { Divider } from './Divider/Divider'
