3423466d3e0e287389abd816a97bbdffb866550d
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-08-16 15:12:49 +0330

    Add Icons

---
+++ app/_icons/Nuts.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ILocalIconType } from './type'
+
+export const Nuts :ILocalIconType = (props) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" {...props}>
+            <g clip-path="url(#clip0_1_196)">
+                <g clip-path="url(#clip1_1_196)">
+                    <path d="M7.68831 1.43113C8.67254 0.777341 9.66756 0.393364 10.2569 0.201505C10.5502 0.105997 10.8615 0.285692 10.9254 0.587473C11.1621 1.7039 11.5223 4.33729 10.2861 6.47839C9.36936 8.06631 8.23722 8.57155 7.16092 8.37789" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M4.97189 4.8379C5.0776 4.48203 5.24271 4.10269 5.47486 3.70062C5.88596 2.98859 6.43247 2.40223 7.01878 1.92371" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M5.62234 5.59626C5.75491 5.17073 5.96798 4.69308 6.27447 4.16224C6.48834 3.79181 6.72485 3.44728 6.97422 3.1283" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10.5023 3.40317C10.3052 4.23958 9.98899 5.1465 9.48653 6.01678C9.18693 6.53571 8.88672 6.95191 8.59103 7.27633" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6.30981 6.83315C6.50861 6.32618 6.84389 5.64862 7.34514 4.78043C8.41148 2.93348 9.73481 1.18078 10.5104 0.203552" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M8.11155 5.91315C7.66739 6.64515 7.29703 7.16762 6.9982 7.51116" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10.7975 0.367432C10.3546 1.48835 9.53999 3.41527 8.52313 5.21093" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M1.18948 8.96191C0.372163 6.65686 1.17455 4.15786 1.59766 3.10893C1.71276 2.82356 2.04956 2.70098 2.32118 2.84559C3.32011 3.37746 5.54311 4.77738 6.39759 7.07088" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M2.0564 8.6515C1.91744 8.26373 1.81038 7.87555 1.72931 7.4935" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M4.20123 4.97403C4.7139 5.66091 5.20412 6.48392 5.54692 7.42578C5.75132 7.98737 5.87685 8.48361 5.93519 8.91751" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M2.15642 4.66989C1.98933 3.98985 1.85771 3.38545 1.76178 2.91653" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M3.21353 8.22549C2.8769 7.2953 2.59146 6.33739 2.35767 5.45743" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M2.07101 2.80231C2.65862 3.88714 3.64381 5.81548 4.36998 7.79771" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6.51303 10.6417C6.16634 11.0644 5.68848 11.3922 5.1001 11.6063C4.22509 11.9248 3.37569 11.8937 2.64373 11.5163C1.89235 11.1289 1.29788 10.4031 0.87432 9.35695C0.821914 9.22751 0.887421 9.08018 1.01867 9.03241L6.57219 7.01109C6.70344 6.96333 6.84833 7.03406 6.89138 7.16693C7.23716 8.23373 7.25035 9.16673 6.93015 9.9435" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M5.10013 11.6063C4.76359 11.7288 4.36958 11.5319 3.96073 11.037C3.80734 10.8513 3.65459 10.6277 3.50293 10.3674" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M4.95419 7.59998C5.11115 8.03123 5.24008 8.4328 5.3407 8.8034" stroke="currentColor" stroke-width="0.3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                    <circle cx="2.73468" cy="9.63947" r="0.175781" fill="currentColor" />
+                    <circle cx="2.62173" cy="10.3495" r="0.175781" fill="currentColor" />
+                    <circle cx="5.36078" cy="9.69119" r="0.175781" fill="currentColor" />
+                    <circle cx="5.61383" cy="10.3864" r="0.175781" fill="currentColor" />
+                    <circle cx="6.06668" cy="9.82794" r="0.175781" fill="currentColor" />
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_1_196">
+                    <rect width="12" height="12" fill="white" />
+                </clipPath>
+                <clipPath id="clip1_1_196">
+                    <rect width="12" height="12" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
