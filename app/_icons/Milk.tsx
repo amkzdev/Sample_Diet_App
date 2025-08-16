69908ca3c0031a4bfe5e28e8276329b27dfb1cb3
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-08-16 13:38:57 +0330

    Add Icons

---
+++ app/_icons/Milk.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ILocalIconType } from './type'
+
+export const Milk: ILocalIconType = (props) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" {...props}>
+            <mask id="mask0_1_133" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="0" y="0" width="12" height="12">
+                <path d="M0 9.53674e-07H12V12H0V9.53674e-07Z" fill="white" />
+            </mask>
+            <g mask="url(#mask0_1_133)">
+                <path d="M3.65157 1.91016H1.22021V0.505617H3.65157V1.91016Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M2.71094 11.4941H11.3905C11.5977 11.4941 11.7656 11.3262 11.7656 11.119V8.45659L11.7654 8.45155L11.7656 8.30076H4.63729" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M4.63733 8.30066H11.7656C11.6959 6.88693 10.856 6.02701 10.4698 5.70402C10.3592 5.61156 10.2055 5.59075 10.0742 5.65037L4.63733 8.11888V8.30066Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M3.27276 1.90848H1.59864L0.71582 3.56641H4.15558L3.27276 1.90848Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M4.1557 3.56737H0.715945L0.704953 3.58802C0.395977 4.16828 0.234375 4.8156 0.234375 5.473V10.7927C0.234375 11.1719 0.541781 11.4941 0.921 11.4941H3.95065C4.32987 11.4941 4.63727 11.1719 4.63727 10.7927V5.473C4.63727 4.8156 4.47567 4.16828 4.16669 3.58802L4.1557 3.56737Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M6.51896 8.87063C6.51896 9.18584 6.26343 9.44141 5.94819 9.44141C5.63298 9.44141 5.37744 9.18584 5.37744 8.87063C5.37744 8.5554 5.63298 8.29986 5.94819 8.29986C6.26343 8.29986 6.51896 8.5554 6.51896 8.87063Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M9.0268 10.7176C9.0268 11.1346 8.68873 11.4727 8.27169 11.4727C7.85466 11.4727 7.5166 11.1346 7.5166 10.7176C7.5166 10.3005 7.85466 9.96246 8.27169 9.96246C8.68873 9.96246 9.0268 10.3005 9.0268 10.7176Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M9.6747 6.63978C9.6747 6.92166 9.44619 7.15018 9.16433 7.15018C8.88245 7.15018 8.65393 6.92166 8.65393 6.63978C8.65393 6.3579 8.88245 6.12938 9.16433 6.12938C9.44619 6.12938 9.6747 6.3579 9.6747 6.63978Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M11.7658 8.45576V9.80583C11.6578 9.86379 11.5346 9.89648 11.4036 9.89648C10.9792 9.89648 10.6353 9.55249 10.6353 9.12837C10.6353 8.70403 10.9792 8.36006 11.4036 8.36006C11.5344 8.36006 11.6578 8.39276 11.7656 8.45072L11.7658 8.45576Z" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M1.19531 10.3418V6.28024" stroke="currentColor" stroke-width="0.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+            </g>
+        </svg>
+    )
+}
